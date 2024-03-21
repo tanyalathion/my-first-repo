662175ee9b11b9a44baad7052d6d356be7e1d07b
tanya <tlathion@icloud.com>
2024-03-21 21:10:04 +0100

    Starting dictionary exercice

---
+++ exercise-dictionaries-in-swift.swift
@@ -0,0 +1,4 @@
+/* Après avoir terminé cet exercice, vous serez en mesure de créer
+et d'utiliser des dictionnaires pour stocker des clés et des valeurs spécifiques.
+Vous serez également en mesure d'utiliser le déballage forcé et la liaison optionnelle
+pour modifier les valeurs d'un dictionnaire. */
