8267abe8129642c7ec03e272e47e4178da641865
tanya <tlathion@icloud.com>
2024-03-24 21:13:48 +0100

    :gear: Setup for loops exercice

---
+++ exercise-using-loops-with-collection-types.swift
@@ -0,0 +1,11 @@
+/* Jusqu'à présent, vous avez appris que les développeurs utilisent des boucles
+pour exécuter continuellement des blocs de code répétés tant qu'une certaine condition est remplie.
+Vous avez également appris que les structures de données telles que les tableaux,
+les tuples et les dictionnaires vous permettent de stocker une collection de valeurs.
+Dans cet exercice, vous vous entraînerez à utiliser des boucles avec ces types de collection.*/
+
+
+
+/* ----------------------- TEST ------------------------------
+Put outputs here
+*/
