2e69a45cbbcb4ddc0161dbe9cfc84e5ba670615d
tanya <tlathion@icloud.com>
2024-03-25 21:02:58 +0100

    :toolbox: Exercice Setup

---
+++ exercise-writing-succinctly-expressive-functions.swift
@@ -0,0 +1,11 @@
+/* Dans cet exercice de fonction, vous pratiquerez le sucre syntaxique Swift en utilisant a(n) :
+  - Valeur par défaut du paramètre.
+  - L'étiquette de l'argument.
+  - Paramètre d'entrée-sortie.
+Vous rendrez une fonction plus expressive et succincte que la version originale.*/
+
+
+
+/* ----------------------- TEST ------------------------------
+Put output here
+*/
