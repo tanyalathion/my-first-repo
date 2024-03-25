bd7438c1c400fd6e6ca5f8bb9cc3056c51dc410c
tanya <tlathion@icloud.com>
2024-03-25 19:14:03 +0100

    :clap: Successful exercice with functions

---
+++ exercise-functions.swift
@@ -0,0 +1,11 @@
+/* Supposons que vous créiez un jeu de rôle dont le protagoniste est un pirate.
+Le but du jeu est de parcourir les sept mers et d'accumuler des lingots d'or en chemin.
+
+Dans cet exercice, vous allez créer une fonction permettant de déverrouiller des coffres au trésor.
+Chaque fois qu'un coffre à trésor est déverrouillé, l'inventaire de barres d'or du joueur augmente de 100. */
+
+
+
+/* ----------------------- TEST ------------------------------
+Insert outputs here
+*/
