7bc47344fb22c2a7276d50423f73f990df949f35
tanya <tlathion@icloud.com>
2024-03-25 22:15:18 +0100

    :building_construction: Refacto exercice

--- exercise-writing-succinctly-expressive-functions.swift
+++ exercise-writing-succinctly-expressive-functions.swift
@@ -1,11 +1,30 @@
 /* Dans cet exercice de fonction, vous pratiquerez le sucre syntaxique Swift en utilisant a(n) :
   - Valeur par défaut du paramètre.
   - L'étiquette de l'argument.
-  - Paramètre d'entrée-sortie.
+  - Paramètre d'entrée-sortie (inout).
 Vous rendrez une fonction plus expressive et succincte que la version originale.*/
 
+var goldBars = 0
 
+func incrementInventory(_ inventory: inout Int, by amount: Int=100){
+  return inventory += amount
+}
+
+incrementInventory(&goldBars)
+print(goldBars)
+incrementInventory(&goldBars)
+print(goldBars)
+incrementInventory(&goldBars)
+print(goldBars)
+incrementInventory(&goldBars, by: 300)
+print(goldBars)
+incrementInventory(&goldBars, by: 50)
+print(goldBars)
 
 /* ----------------------- TEST ------------------------------
-Put output here
+100
+200
+300
+600
+650
 */
