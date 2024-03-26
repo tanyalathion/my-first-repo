3619bbcba3b01ae76c59531d4f16e205932cb772
tanya <tlathion@icloud.com>
2024-03-26 16:39:26 +0100

    :gear: Setup exercice file

---
+++ exercise-use-functions-to-modularize-a-program.swift
@@ -0,0 +1,11 @@
+/* ----------------------- SENARIO ------------------------------
+Le pirate cherche à dépenser les lingots d'or accumulés.
+Après avoir dépensé les lingots d'or, les joueurs veulent être informés des lingots d'or restants.
+Pour ce faire, vous écrirez une fonction avec un gestionnaire d'achèvement.
+----------------------------------------------------------- */
+
+
+
+/* ----------------------- TEST ------------------------------
+
+----------------------------------------------------------- */
