2e123c538e2a38cf79aabb37864d6ba423e83ac6
tanya <tlathion@icloud.com>
2024-03-26 16:20:21 +0100

    :gear: Setup exercice file

---
+++ exercise-using-closures.swift
@@ -0,0 +1,18 @@
+/* ----------------------- SENARIO ------------------------------
+Vous souhaitez faire circuler les instructions de déverrouillage du coffre au trésor dans votre base de code.
+Pour étendre cette fonctionnalité, vous allez abstraire les instructions dans une fermeture.
+Une fermeture vous permet d'enregistrer des instructions dans une variable/constante.
+Vous utiliserez une constante au lieu d'une variable car vous n'aurez besoin
+d'assigner la fermeture qu'une seule fois dans cet exercice.
+Grâce à la fonctionnalité contenue dans une constante,
+vous pouvez faire circuler la constante comme n'importe quelle autre constante.
+Outre l'exécution de blocs de code, vous pouvez enregistrer une fermeture dans une variable/constante.
+La fermeture est donc plus polyvalente qu'une fonction.
+L'enregistrement d'une fermeture dans une variable/constante est un exemple de sa polyvalence.
+
+Dans cet exercice, vous vous entraînerez à écrire une fermeture pour déverrouiller un coffre au trésor.
+----------------------------------------------------------- */
+
+/* ----------------------- TEST ------------------------------
+Put outputs here
+----------------------------------------------------------- */
