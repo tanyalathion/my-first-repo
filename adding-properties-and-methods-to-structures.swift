8c7b69a0c7b3aec6910c0a0257a9f89d3dcd88cd
tanya <tlathion@icloud.com>
2024-03-27 15:16:28 +0100

    :gear: Setup practice file on structures

---
+++ adding-properties-and-methods-to-structures.swift
@@ -0,0 +1,13 @@
+/* ----------------------- SENARIO ------------------------------
+Supposons que vous souhaitiez représenter des informations
+sur différentes voitures dans le code.
+Pour ce faire, vous pouvez créer une structure dotée de propriétés
+permettant de regrouper des informations connexes.
+----------------------------------------------------------- */
+
+
+
+/* ----------------------- TEST ------------------------------
+La couleur de la voiture est : rose.
+La voiture bleue roule à une vitesse maximale de... 100 !
+----------------------------------------------------------- */
