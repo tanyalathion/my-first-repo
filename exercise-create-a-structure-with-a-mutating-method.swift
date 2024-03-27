dcfbd472fa9f2a80eebf18cd743764033d23c8da
tanya <tlathion@icloud.com>
2024-03-27 21:30:26 +0100

    :gear: Setup practice file on mutating methods

---
+++ exercise-create-a-structure-with-a-mutating-method.swift
@@ -0,0 +1,14 @@
+/* ----------------------- SENARIO ------------------------------
+Chez Little Lemon, les clients peuvent réserver une table de différentes manières.
+Vous êtes chargé de créer une structure qui contiendra les informations de réservation pour chaque personne,
+y compris leur nom et le numéro de la table.
+Il doit également être possible de mettre à jour le nom de la personne
+pour tenir compte de tout changement de réservation.
+----------------------------------------------------------- */
+
+
+
+/* ----------------------- TEST ------------------------------
+TableReservation(name: "John", tableNumber: 1)
+TableReservation(name: "Maria", tableNumber: 1)
+----------------------------------------------------------- */
