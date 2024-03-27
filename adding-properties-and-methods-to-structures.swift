d101f6d1be47c4bedd2c5c71813670e998838694
tanya <tlathion@icloud.com>
2024-03-27 15:27:17 +0100

    :blue_car: Successfull practice on structures

--- adding-properties-and-methods-to-structures.swift
+++ adding-properties-and-methods-to-structures.swift
@@ -5,9 +5,25 @@ Pour ce faire, vous pouvez créer une structure dotée de propriétés
 permettant de regrouper des informations connexes.
 ----------------------------------------------------------- */
 
+struct Car {
+    var color: String
+    var name: String
+    var topSpeed: Int
+    var mileage: Int
 
+    func drive() {
+        print("La voiture \(color) roule à une vitesse maximale de: \(topSpeed)!")
+  }
+}
+
+var newCar = Car(color: "bleu", name: "Porsche", topSpeed: 100, mileage: 5000)
+var myCar = Car(color: "rose", name: "Cadillac", topSpeed: 100, mileage: 4000)
+var smallCar = Car(color: "orange", name: "Twingo", topSpeed: 80, mileage: 3000)
+
+print("La couleur de ma voiture est: \(myCar.color).")
+newCar.drive()
 
 /* ----------------------- TEST ------------------------------
 La couleur de la voiture est : rose.
-La voiture bleue roule à une vitesse maximale de... 100 !
+La voiture bleue roule à une vitesse maximale de: 100 !
 ----------------------------------------------------------- */
