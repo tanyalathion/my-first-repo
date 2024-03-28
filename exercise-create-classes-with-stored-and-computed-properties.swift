3863d0792614e6b1e7d14a2522e945dcdc02b6c2
tanya <tlathion@icloud.com>
2024-03-28 13:08:36 +0100

    :gear: Setup practice file on properties types

---
+++ exercise-create-classes-with-stored-and-computed-properties.swift
@@ -0,0 +1,15 @@
+/* ----------------------- SENARIO ------------------------------
+La conception de l'application Little Lemon est riche en images et autres éléments
+qui donnent aux utilisateurs plus de détails sur les repas, les promotions et les rabais
+plutôt qu'un simple texte.
+Votre tâche consiste à écrire une classe qui représente un fichier interne de l'application
+et qui peut produire un nom de fichier complet afin de le charger correctement ultérieurement
+pour que les utilisateurs puissent le consulter.
+Pour ce faire, vous utiliserez des propriétés stockées et calculées dans une classe.
+----------------------------------------------------------- */
+
+
+
+/* ----------------------- TEST ------------------------------
+Put outputs here
+----------------------------------------------------------- */
