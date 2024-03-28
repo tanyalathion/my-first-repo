25c8cab2d8723d876bd419ef640a64717c2818d0
tanya <tlathion@icloud.com>
2024-03-28 13:30:28 +0100

    :clap:Successfull exercice on classes and properties types

--- exercise-create-classes-with-stored-and-computed-properties.swift
+++ exercise-create-classes-with-stored-and-computed-properties.swift
@@ -8,8 +8,28 @@ pour que les utilisateurs puissent le consulter.
 Pour ce faire, vous utiliserez des propriétés stockées et calculées dans une classe.
 ----------------------------------------------------------- */
 
+class LocalFile {
+    /*propriétés stockeés*/
+    let name: String
+    let fileExtension: String
+    /*La fonction constructeur de la classe n'a pas de mot-clé func au début.
+    Son nom est toujours init*/
+    init (name: String, fileExtension: String) {
+        /*si un argument de la méthode du constructeur porte le même nom que la propriété de la classe,
+        vous devez utiliser le mot-clé self lorsque vous faites référence à la propriété de la classe*/
+        self.name = name
+        self.fileExtension = fileExtension
+    }
+        /*propriété calculée pour pouvoir renvoyer un nom de fichier complet*/
+    var fullFileName: String {
+        return name + "." + fileExtension
+    }
+}
 
+let file = LocalFile(name: "image", fileExtension: "png")
+
+print(file.fullFileName)
 
 /* ----------------------- TEST ------------------------------
-Put outputs here
+image.png
 ----------------------------------------------------------- */
